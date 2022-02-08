7a79dbd1201fca144d74d6e6e46d14efa461efff
Gusb3ll <Kitpipat.0647@gmail.com>
2022-02-08 21:29:20 +0700

    ❌ remove unrelated words

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,7 @@
     <div id="index_container" class="flex h-screen z-100">
       <div class="m-auto">
         <div class="p-8">
-          <!-- <img src="~/assets/text.svg" class="glitch w-220"> -->
-          <h1 class="glitch text-5xl text-white font-thin" />
+          <h1 class="glitch md:text-5xl text-2xl text-white font-thin" />
         </div>
       </div>
     </div>
@@ -34,7 +33,7 @@ export default {
       await this.sleep(1500)
       // eslint-disable-next-line no-undef
       const writer = GlitchedWriter.create('.glitch', 'encrypted', () => {})
-      const phrases = ['Abandon the old dying world', 'Embrace the new existence', 'The place of the dream', 'The paradise', 'Hidden in the virtual world', 'Coming soon - Q4 2022']
+      const phrases = ['Reset the old world', 'Embrace this new existence', 'Coming soon - Q4 2022']
       writer.queueWrite(phrases, 2000, false)
     }
   }
